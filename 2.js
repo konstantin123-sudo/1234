6923687a757a8edc31852ac6e40a5a0b9bf2a9bc
konstantin123-sudo <81141905+konstantin123-sudo@users.noreply.github.com>
2021-03-24 12:35:22 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,4 @@
+//BEGIN
+let message = ("Жить хорошо, а хорошо жить еще лучше");
+document.writeln(message);
+//END
---
+++ 4.js
@@ -0,0 +1,8 @@
+let a = +prompt("Введите число a", 3);
+let b = +prompt("Введите число b", 4);
+let slozhenie = a + b;
+let proizvedenije = a * b;
+let srarifm = (a + b) / 2;
+alert(`Сумма a и b равна: ${slozhenie}`);
+alert(`Произведение a и b равно: ${proizvedenije}`);
+alert(`Среднее арифметическое a и b равно: ${srarifm}`);
